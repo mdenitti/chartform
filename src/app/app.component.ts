18b7c7303189cf7e2125fdf67b398a5f8bd97c45
mdenitti <massimodn77@gmail.com>
2022-12-16 11:18:40 +0100

    introduction to ngmodel and basic usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+addUsers() {
+    // add the user to the list
+    this.users.push({name: this.name, email: this.email});
+    // clear the form
+    this.name = '';
+    this.email = '';
+}
   title = 'chartform';
+  name = '';
+  email = '';
+
+  // create a list of users
+  users = [
+    {name: 'John', email: 'john@johnny.com'},
+    {name: 'Jane', email: 'jane@jannie.com'},
+    {name: 'Jack', email: 'jack@jacky.com'}
+  ]
+
+  // transform the name to uppercase and lowercase instantly
+  // because the name is a two-way binding
+  transformToUpperCase() {
+    this.name = this.name.toUpperCase();
+    this.email = this.email.toUpperCase();
+  }
+
+  transformToLowerCase() {
+    this.name = this.name.toLowerCase();
+    this.email = this.email.toLowerCase();
+  }
 }
