42dddc559abc8a1bdd62741622912170bfe54c8f
mdenitti <massimodn77@gmail.com>
2022-12-19 13:44:50 +0100

    dynamic chart implementation

--- src/app/my-chart/my-chart.component.ts
+++ src/app/my-chart/my-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from "@angular/core";
+import { Component, ViewChild, OnInit } from "@angular/core";
 import {
   ChartComponent,
   ApexAxisChartSeries,
@@ -23,15 +23,29 @@ export type ChartOptions = {
 })
 export class MyChartComponent {
   @ViewChild("chart") chart!: ChartComponent;
-  chartOptions: ChartOptions;
+  chartOptions: ChartOptions | any;
   // chartOptions!: Partial<ChartOptions>; // With partial -> objecttypes are optional
 
+  // Backlog items:
+  // - Create a fetch method to get the data from the backend
+  // - Allocate data to the chartOptions (this)
+  // - map data to the keys in the chartOptions
+  // - Execute the method in the constructor
+
+
   constructor() { 
-    this.chartOptions = {
+  }
+
+  fetchUsers = () => {
+    fetch('https://jsonplaceholder.typicode.com/users').then(
+      res => res.json()
+    ).then(
+      data => {
+        this.chartOptions = {
       series: [
         {
           name: "My X-mas presents",
-          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+          data: data.map((user:any) => user.address.geo.lat)
         }
       ],
       colors: ['#CC0000'],
@@ -44,12 +58,16 @@ export class MyChartComponent {
         text: "Have a nice X-mas - and don't be a naught boy/girl"
       },
       xaxis: {
-        categories: ["Jan", "Feb",  "Mar",  "Apr",  "May",  "Jun",  "Jul",  "Aug", "Sep"]
+        categories: data.map((user:any) => user.name)
       }
     };
-  
+      }
+    ).catch()
   }
 
- 
+  ngOnInit(): void {
+    this.fetchUsers();
+    console.log(this.chartOptions);
+  }
 
 }
